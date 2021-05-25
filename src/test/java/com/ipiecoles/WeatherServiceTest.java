44a39e4eb9a3ae4f8ebebcd67046172b6a2d422f
antoinebouard <antoinebouardain@gmail.com>
2021-05-25 17:30:55 +0200

    tu

--- src/test/java/com/ipiecoles/WeatherServiceTest.java
+++ src/test/java/com/ipiecoles/WeatherServiceTest.java
@@ -15,16 +15,16 @@ public class WeatherServiceTest {
     @Test
     public void testGetWeatherOfTheCity() throws Exception{
         //Given
-        Mockito.when(webUtils.getPageContents(Mockito.anyString())).thenReturn("{\"coord\":{\"lon\":2.3488,\"lat\":48.8534},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"nuageux\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":288.05,\"feels_like\":287.35,\"temp_min\":286.08,\"temp_max\":289.92,\"pressure\":1018,\"humidity\":67},\"visibility\":10000,\"wind\":{\"speed\":4.02,\"deg\":224,\"gust\":9.83},\"clouds\":{\"all\":75},\"dt\":1621950361,\"sys\":{\"type\":2,\"id\":2012208,\"country\":\"FR\",\"sunrise\":1621915050,\"sunset\":1621971476},\"timezone\":7200,\"id\":2988507,\"name\":\"Paris\",\"cod\":200}");
+        Mockito.when(webUtils.getPageContents(Mockito.anyString())).thenReturn("{\"coord\":{\"lon\":2.3488,\"lat\":48.8534},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"couvert\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":15.22,\"feels_like\":14.31,\"temp_min\":14.1,\"temp_max\":16.23,\"pressure\":1018,\"humidity\":58},\"visibility\":10000,\"wind\":{\"speed\":2.24,\"deg\":220,\"gust\":7.6},\"clouds\":{\"all\":90},\"dt\":1621956068,\"sys\":{\"type\":2,\"id\":2012208,\"country\":\"FR\",\"sunrise\":1621915050,\"sunset\":1621971476},\"timezone\":7200,\"id\":2988507,\"name\":\"Paris\",\"cod\":200}");
         //When
         Weather weather = new WeatherService(webUtils).getWeatherOfTheCity("Paris");
         //Then
         Assertions.assertThat(weather).isNotNull();
         Assertions.assertThat(weather.getSunsetDate()).isEqualTo("21:37");
         Assertions.assertThat(weather.getsunriseDate()).isEqualTo("5:57");
-        Assertions.assertThat(weather.getDescription()).isEqualTo("nuageux");
-        Assertions.assertThat(weather.getHumidity()).isEqualTo(67);
-        Assertions.assertThat(weather.getTemp()).isEqualTo(288.05);
-        Assertions.assertThat(weather.getId()).isEqualTo(803);
+        Assertions.assertThat(weather.getDescription()).isEqualTo("couvert");
+        Assertions.assertThat(weather.getHumidity()).isEqualTo(58);
+        Assertions.assertThat(weather.getTemp()).isEqualTo(15.22);
+        Assertions.assertThat(weather.getId()).isEqualTo(804);
     }
 }
